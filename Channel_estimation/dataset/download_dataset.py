4630dbcdb41d17b1c71d710bb3e9a5627c42431d
WesinAlves <wesinec@gmail.com>
2020-04-24 18:00:51 -0300

    download dataset

--- Channel_estimation/dataset/download_dataset.py
+++ Channel_estimation/dataset/download_dataset.py
@@ -3,12 +3,12 @@ Download dataset for channel estimation challenge
 '''
 import wget
 
-URL = 'https://nextcloud.lasseufpa.org/remote.php/webdav/5GM/Channel_estimation/train.hdf5'
+URL = 'https://nextcloud.lasseufpa.org/s/reANxekeS4dCppD/download'
 TRAIN_FILE_NAME = wget.download(URL)
 
 print(f'{TRAIN_FILE_NAME} was downloaded successfully!')
 
-URL = 'https://nextcloud.lasseufpa.org/remote.php/webdav/5GM/Channel_estimation/test.hdf5'
+URL = 'https://nextcloud.lasseufpa.org/s/GdXnSpLCHHytPP2/download'
 TEST_FILE_NAME = wget.download(URL)
 
 print(f'{TEST_FILE_NAME} was downloaded successfully!')
